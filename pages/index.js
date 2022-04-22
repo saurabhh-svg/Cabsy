a15f24d404612c4cab2987ffc88bfb382c1fc52c
Saurabh Singh <singhsaurabh2901@gmail.com>
2022-04-22 21:59:49 +0530

    map

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import tw from "tailwind-styled-components";
-import Map from "./components/map";
+import Map from "./components/Map";
 import Link from "next/link";
 import { auth } from "../firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
